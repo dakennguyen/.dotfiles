c800097c6894a6ca73a691b5cec83e240b5b33bf
Khoa Nguyen <dakennguyen@gmail.com>
2025-08-17 17:48:01 +0100

    Add alias

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -20,12 +20,15 @@ export GPG_TTY=$TTY
 # Aliases
 #---------------------------------------------------------------------------
 alias v="source ~/.zshenv && \$EDITOR"
+alias sv="source ~/.zshenv && sudo $(command -v $EDITOR)"
 alias cf="cd ~/.dotfiles && v"
 alias cfn="cd ~/.config/nvim && v"
 alias note="nvim '$CLOUD/Notes'"
 alias c="clear"
 alias q="exit"
 alias s="source ~/.zshenv && source ~/.zshrc"
+alias dk="docker"
+alias dkc="docker compose"
 
 [ -f ~/.dotfiles/zsh/nnn.zsh ] && source ~/.dotfiles/zsh/nnn.zsh
 command -v fzf >/dev/null && source <(fzf --zsh)
